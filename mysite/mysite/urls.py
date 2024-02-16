b5e04baa13e06beece6efb37c408fa80f2215ea9
frisbeedata <richardgross345@gmail.com>
2024-02-16 00:30:20 +0000

    First Django application

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from polls.views import site_index
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("", site_index), # Default
 ]
